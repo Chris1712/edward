c9e7cf857ef741ca801ae47dce76c294ff40b2ea
Chris Turner <Chris.Turner@fidessa.com>
2019-09-02 14:12:51 +0100

    Pass tests for filenames and arg casing

--- src/main/java/personal/chris/Edward.java
+++ src/main/java/personal/chris/Edward.java
@@ -5,26 +5,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 public class Edward {
+
+    public enum OperationModes {DECODE, ENCODE}
+
     public static void main(String[] args) throws IOException {
         validateArgs(args);
-        readFile(args[0]);
+        Path readPath = Paths.get(args[1]);
 
+        if (args[0].equals("-e")) {
+            byte[] fileBytes = Files.readAllBytes(readPath);
+            String fileString = Base64.getEncoder().encodeToString(fileBytes);
+//            Path writePath =
+            //  Whatever the file is, convert to base 64 string, put some stuff above it, and put in .edward
 
+        } else if (args[0].equals("-d")) {
+            //  Verify .edward file, parse top bit, read in base 64 and then output
+        }
     }
 
-    private static void validateArgs(String[] args) throws IllegalArgumentException {
+    public static void validateArgs(String[] args) throws IllegalArgumentException {
         if (args == null) {
             throw new IllegalArgumentException("Null argument supplied");
         }
-        if (args.length != 1) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Incorrect number of arguments supplied");
         }
-    }
+        if (!args[0].matches("-[ed]")) {
+            throw new IllegalArgumentException("Incorrect number of arguments supplied");
+        }
+        if (args[0].equals("-d") && !args[1].matches(".*\\.edward")) {
+            throw new IllegalArgumentException("Can only decode .edward files");
+        }
+        if (args[0].equals("-e") && args[1].matches(".*\\.edward")) {
+            throw new IllegalArgumentException("Cannot encode .edward files");
+        }
 
-    private static byte[] readFile(String path) throws IOException {
-        Path filePath = Paths.get(path);
-        return Files.readAllBytes(filePath);
     }
 }
