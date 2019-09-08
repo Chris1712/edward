fe1ba21d4b13e804d097ce808920c95bb6d9d797
Chris <christurner5@gmail.com>
2019-09-08 19:21:55 +0100

    Pass tests for decoder

--- src/main/java/personal/chris/edward/services/Decoder.java
+++ src/main/java/personal/chris/edward/services/Decoder.java
@@ -1,6 +1,13 @@
 package personal.chris.edward.services;
 
 import org.springframework.stereotype.Service;
+import personal.chris.edward.Messages;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 /***
  * Service for parsing arguments supplied to Edward and determining methods required
@@ -8,8 +15,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class Decoder {
 
-    public void decodeFileToFile(String filePath) {
+    public void decodeFileToFile(String readPath) {
+        String fileContent;
+        try {
+            fileContent = new String(Files.readAllBytes(Paths.get(readPath)));
+        } catch (IOException ex) {
+            throw new IllegalArgumentException(Messages.COULD_NOT_READ + readPath);
+        }
+        byte[] fileBytes = Base64.getDecoder().decode(fileContent);
 
+        String writePath = readPath.substring(0, readPath.length()-7);
+        try (FileOutputStream stream = new FileOutputStream(writePath)) {
+            stream.write(fileBytes);
+        } catch (IOException ex) {
+            throw new IllegalArgumentException(Messages.COULD_NOT_WRITE + writePath);
+        }
+        System.out.println(Messages.fileDecodedToFile(readPath, writePath));
     }
-
 }
