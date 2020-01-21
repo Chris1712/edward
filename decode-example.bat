591b571c0d163b5f8057121c1640af222307230b
Chris <christurner5@gmail.com>
2020-01-21 22:19:49 +0000

    Set version and remove unneeded examples

--- decode-example.bat
+++
@@ -1,2 +0,0 @@
-:: Writes the img2.png.edward text file to img2.png
-java -jar target\edward-1.0-SNAPSHOT.jar -d example\img2.png.edward
--- encode-example.bat
+++
@@ -1,2 +0,0 @@
-:: Writes the img.png image file to img.png.edward
-java -jar target\edward-1.0-SNAPSHOT.jar -e example\img.png
